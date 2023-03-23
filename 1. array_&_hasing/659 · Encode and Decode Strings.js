76c5be6da5991e74124c761c293f4091085e6e6d
hridoy43 <hridoy43@gmail.com>
2023-03-24 01:46:43 +0600

    new solution added to 659 num

--- 1. array_&_hasing/659 · Encode and Decode Strings.js
+++ 1. array_&_hasing/659 · Encode and Decode Strings.js
@@ -16,6 +16,31 @@ var decode = (str) => {
     return str.split('_:_')
 }
 
+// More sophisticated approach -> O(n)
+var encode = (strs) => {
+    let res = ""
+    for(str of strs) {
+        res += str.length + '#' + str // example: 5#lint
+    }
+    return res
+}
+
+var decode = (str) => {
+    const res = []
+    let i = 0;
+
+    while (i < str.length) {
+        let j = i;
+        while(str[j] !== '#') {
+            j += 1;
+        }
+        const length = parseInt(str[j-1])
+        res.push(str.substring(j+1, j+length+1))
+
+        i = j + 1 + length
+    }
+    return res
+}
 
 // console.log(encode(testInputs[0]))
 // console.log(decode(encode(testInputs[0])))
