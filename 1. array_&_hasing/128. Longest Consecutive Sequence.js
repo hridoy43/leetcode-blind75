dc07b007237c24fec25d412c9556c74eeffdfe58
hridoy43 <hridoy43@gmail.com>
2023-03-26 02:35:56 +0600

    another solution added to 128. Longest Consecutive Sequence

--- 1. array_&_hasing/128. Longest Consecutive Sequence.js
+++ 1. array_&_hasing/128. Longest Consecutive Sequence.js
@@ -31,6 +31,24 @@ var longestConsecutive = function(nums) {
     return longestConsecutive
 };
 
+
+// Another Approach -> O(n)
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums)
+    let longest = 0;
+
+    for(let num of nums){
+        if(!numSet.has(num - 1)){
+            length = 0;
+            while(numSet.has(num + length)){
+                length += 1;
+            }
+            longest = Math.max(longest, length)
+        }
+    }
+    return longest
+};
+
 // console.log(longestConsecutive(testInputs[0]))
 // console.log(longestConsecutive(testInputs[1]))
 // console.log(longestConsecutive(testInputs[2]))
