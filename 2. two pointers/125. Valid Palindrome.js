e556a2ade69e7945f4dfe8c7a866c26e3732453e
hridoy43 <hridoy43@gmail.com>
2023-03-30 23:51:35 +0600

    125. Valid Palindrome's solution updated

--- 2. two pointers/125. Valid Palindrome.js
+++ 2. two pointers/125. Valid Palindrome.js
@@ -11,8 +11,33 @@ const testCases = [
 // brute force
 var isPalindrome = function(s) {
     const newString = (s.match(/[a-zA-Z0-9]/g)?.join('') ?? "").toLowerCase()
-    if(newString=== newString?.split('')?.reverse()?.join('')) return true
-    return false
+    return newString === newString?.split('')?.reverse()?.join('')
+};
+
+
+// Better Approach without extra space O(n)
+const isAlphaNumeric = (char) => { 
+    return ((char.charCodeAt() >= 'A'.charCodeAt()) && char.charCodeAt() <= 'Z'.charCodeAt()) ||
+    ((char.charCodeAt() >= 'a'.charCodeAt()) && char.charCodeAt() <= 'z'.charCodeAt()) ||
+    ((char.charCodeAt() >= '0'.charCodeAt()) && char.charCodeAt() <= '9'.charCodeAt())
+ }
+
+ var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while(left < right){
+        while(left < right && !isAlphaNumeric(s[left])) {
+            left++;
+        }
+        while(left < right && !isAlphaNumeric(s[right])) {
+            right--;
+        }
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) return false
+        left++;
+        right--;
+    }
+    return true
 };
 
 // console.log(isPalindrome(testCases[0]))
