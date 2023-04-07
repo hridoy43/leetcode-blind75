0d9f929d250321e78f9aa77661154e7bce6228aa
hridoy43 <hridoy43@gmail.com>
2023-04-08 00:20:57 +0600

    3. Longest Substring Without Repeating Characters

---
+++ 3. sliding window/3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,44 @@
+// 3. Longest Substring Without Repeating Characters
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+const testCases = [
+    "abcabcbb", // output -> 3
+    "bbbbb", // 1
+    "pwwkew", // 3
+    "au", // 2
+    "dvdf", // 3
+]
+
+
+/**
+ * Brute Force
+ */
+var lengthOfLongestSubstring = function(s) {
+    if (s.length <= 1) return s.length
+
+    let tempSubString = '';
+    let longestSubstring = '';
+    let left = 0;
+    let right = 1;
+
+    while(right < s.length) {
+        tempSubString = s.slice(left,right);
+
+        if (!tempSubString.includes(s[right])) {
+            tempSubString = s.slice(left,right+1);
+            right++;
+        } else {
+            if (tempSubString.length > longestSubstring.length){
+                longestSubstring = tempSubString;
+            }
+            left++;
+        }
+    }
+    return longestSubstring.length > tempSubString.length ? longestSubstring.length : tempSubString.length;
+};
+
+// console.log(lengthOfLongestSubstring(testCases[0]))
+// console.log(lengthOfLongestSubstring(testCases[1]))
+// console.log(lengthOfLongestSubstring(testCases[2]))
+// console.log(lengthOfLongestSubstring(testCases[3]))
+// console.log(lengthOfLongestSubstring(testCases[4]))
